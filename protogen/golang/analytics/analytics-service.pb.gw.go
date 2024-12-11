79d01949739720ef876f38ac2d66e0417d11bc50
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-12-11 16:33:50 +0100

    analytics fix

--- protogen/golang/analytics/analytics-service.pb.gw.go
+++ protogen/golang/analytics/analytics-service.pb.gw.go
@@ -513,6 +513,10 @@ func local_request_AnalyticsService_UpdateAnalyticsForEachTaskHowMuchTimeWasInEa
 
 }
 
+var (
+	filter_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn_0 = &utilities.DoubleArray{Encoding: map[string]int{"projectId": 0, "userId": 1}, Base: []int{1, 1, 2, 0, 0}, Check: []int{0, 1, 1, 2, 3}}
+)
+
 func request_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn_0(ctx context.Context, marshaler runtime.Marshaler, client AnalyticsServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AnalyticsForEachUserOnWhichTasksIsHeCurrentlyOnRequest
 	var metadata runtime.ServerMetadata
@@ -542,6 +546,13 @@ func request_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn
 	protoReq.UserId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "userId", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -578,6 +589,13 @@ func local_request_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurre
 	protoReq.UserId, err = runtime.String(val)
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "userId", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_AnalyticsService_GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.GetAnalyticsForEachUserOnWhichTasksIsHeCurrentlyOn(ctx, &protoReq)
